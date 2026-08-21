260e7ef7476b98097ffe0042150862a7e4145cf3
VinicaoMDM <vici230313@gmail.com>
2026-08-21 15:19:11 -0300

    feat: cadastra fonte INEP

---
+++ sql/005_seed_core_fonte.sql
@@ -0,0 +1,18 @@
+﻿-- EduData Brasil
+-- Cadastro inicial de fontes de dados
+
+INSERT INTO core.fonte (
+    nome,
+    sigla,
+    descricao,
+    url,
+    orgao_responsavel
+)
+VALUES (
+    'Instituto Nacional de Estudos e Pesquisas Educacionais Anísio Teixeira',
+    'INEP',
+    'Autarquia federal responsável por avaliações, exames, pesquisas estatísticas e indicadores educacionais no Brasil.',
+    'https://www.gov.br/inep/',
+    'Ministério da Educação'
+)
+ON CONFLICT DO NOTHING;
