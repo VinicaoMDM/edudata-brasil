60b79cb6f3835a688391a1e9ea419748a8a4547d
VinicaoMDM <vici230313@gmail.com>
2026-08-21 14:13:16 -0300

    feat: cria schemas da arquitetura de dados

---
+++ sql/001_create_schemas.sql
@@ -0,0 +1,10 @@
+-- EduData Brasil
+-- Criação das camadas de dados
+
+CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE SCHEMA IF NOT EXISTS core;
+
+CREATE SCHEMA IF NOT EXISTS analytics;
