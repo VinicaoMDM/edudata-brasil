fccc0b795f56c0e29277b2e7732f300bbac1409f
VinicaoMDM <vici230313@gmail.com>
2026-08-24 08:51:53 -0300

    feat: cadastra versao 2023 do censo escolar

---
+++ sql/009_seed_core_dataset_versao.sql
@@ -0,0 +1,26 @@
+﻿-- EduData Brasil
+-- Cadastro da versão 2023 do Censo Escolar da Educação Básica
+
+INSERT INTO core.dataset_versao (
+    dataset_id,
+    versao,
+    periodo_referencia,
+    url_download,
+    formato,
+    data_extracao
+)
+SELECT
+    d.id,
+    '2023',
+    '2023',
+    'https://www.gov.br/inep/pt-br/areas-de-atuacao/pesquisas-estatisticas-e-indicadores/censo-escolar',
+    'ZIP',
+    NULL
+FROM core.dataset d
+WHERE d.nome = 'Censo Escolar da Educação Básica'
+  AND NOT EXISTS (
+      SELECT 1
+      FROM core.dataset_versao dv
+      WHERE dv.dataset_id = d.id
+        AND dv.versao = '2023'
+  );
